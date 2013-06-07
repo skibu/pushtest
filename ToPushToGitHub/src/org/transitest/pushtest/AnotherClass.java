0755f7118ef54d4c0742a6012218de1fa9b18ded
Skibu Smith <skibu@smith.net>
2013-06-06 21:22:45 -0700

    Added second class for testing

---
+++ ToPushToGitHub/src/org/transitest/pushtest/AnotherClass.java
@@ -0,0 +1,13 @@
+package org.transitest.pushtest;
+
+public class AnotherClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
